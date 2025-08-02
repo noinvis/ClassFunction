6db33425599e02a47e0b25a24546a1ab946de08c
noinvis <lazizbek.sn@gmail.com>
2025-08-02 14:58:03 +0500

    First

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './components/header/Header'
+import Hero from './components/hero/Hero'
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  )
+}
+
+export default App
